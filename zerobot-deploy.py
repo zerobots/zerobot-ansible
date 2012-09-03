7ca2589a12c4db97ec9b463319d6adcd92ee9377
Charles Blaxland <charles.blaxland@gmail.com>
2012-09-03 22:18:48 +1000

    Call ansible to run deploy playbook

--- zerobot-deploy.py
+++ zerobot-deploy.py
@@ -3,6 +3,7 @@
 import json
 import re
 import os
+from subprocess import call
 import zipfile
 import boto
 from boto.s3.key import Key
@@ -36,6 +37,8 @@ def main():
   bucket = module.params['bucket']
   dest = module.params['dest']
 
+  changed = False
+
   conn = boto.connect_s3()
   s3_bucket = conn.get_bucket(bucket)
 
@@ -46,15 +49,25 @@ def main():
     dest_folder = dest + '/%s/%s/%s' % (project, app, release_folder_name)
     dest_zip_name = dest_folder + '/' + zip_name
     if not os.path.exists(dest_folder):
+      changed = True
       os.makedirs(dest_folder)
 
-    release_key = Key(s3_bucket)
-    release_key.key = '%s/%s' % (release_folder_name, zip_name)
-    release_key.get_contents_to_filename(dest_zip_name)
-    with zipfile.ZipFile(dest_zip_name) as dest_zip:
-      dest_zip.extractall(dest_folder)
+    if not os.path.exists(dest_zip_name):
+      changed = True
+      release_key = Key(s3_bucket)
+      release_key.key = '%s/%s' % (release_folder_name, zip_name)
+      release_key.get_contents_to_filename(dest_zip_name)
 
-  module.exit_json(changed=True)
+    if not os.path.exists(dest_folder + '/deploy'):
+      changed = True
+      with zipfile.ZipFile(dest_zip_name) as dest_zip:
+        dest_zip.extractall(dest_folder)
+
+    ansible_command_line = 'ansible-playbook -c local -e "app_home=\'%s\'" %s/deploy/deploy.yml' % (dest_folder, dest_folder)
+    print ansible_command_line
+    call([ansible_command_line], shell = True)
+
+  module.exit_json(changed = changed)
 
 
 # include magic from lib/ansible/module_common.py
