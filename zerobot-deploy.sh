bd7b3697e313c302e6fe31c86a88faba5cb099f5
Charles Blaxland <charles.blaxland@gmail.com>
2012-09-16 18:36:13 +1000

    Created shell script wrapper for doing deploys

---
+++ zerobot-deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $scriptDir
+ansible 127.0.0.1 -c local --module-path=$scriptDir -m zerobot-deploy2 -a 'project=$1 app=$2 bucket=$3' >> /var/log/zerobot-deploy.log
