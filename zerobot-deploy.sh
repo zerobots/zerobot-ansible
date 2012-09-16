2c40ac1ca22913eb802e0780762a9bc31ab8f5a7
Charles Blaxland <charles.blaxland@gmail.com>
2012-09-16 20:51:53 +1000

    Changed vars param to double quotes so params get substituted

--- zerobot-deploy.sh
+++ zerobot-deploy.sh
@@ -3,4 +3,4 @@
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $scriptDir
-ansible 127.0.0.1 -c local --module-path=$scriptDir -m zerobot-deploy2 -a 'project=$1 app=$2 bucket=$3' >> /var/log/zerobot-deploy.log
+ansible 127.0.0.1 -c local --module-path=$scriptDir -m zerobot-deploy2 -a "project=$1 app=$2 bucket=$3" >> /var/log/zerobot-deploy.log 2>&1
