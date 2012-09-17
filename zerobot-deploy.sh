449c1413e8c800d8fdefc51e2ef4a57d3b58d85d
Charles Blaxland <charles.blaxland@gmail.com>
2012-09-17 22:20:44 +1000

    Moved the new deploy module over to zerobot-deploy

--- zerobot-deploy.sh
+++ zerobot-deploy.sh
@@ -4,5 +4,5 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $scriptDir
 echo `date` Beginning deploy of $1-$2 from $3 >> /var/log/zerobot-deploy.log
-ansible 127.0.0.1 -c local --module-path=$scriptDir -m zerobot-deploy2 -a "project=$1 app=$2 bucket=$3" >> /var/log/zerobot-deploy.log 2>&1
+ansible 127.0.0.1 -c local --module-path=$scriptDir -m zerobot-deploy -a "project=$1 app=$2 bucket=$3" >> /var/log/zerobot-deploy.log 2>&1
 echo `date` Deploy of $1-$2 from $3 complete >> /var/log/zerobot-deploy.log
