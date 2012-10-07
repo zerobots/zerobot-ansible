577d829348b966c1aa9c093964f563442261b7dc
Charles Blaxland <charles.blaxland@gmail.com>
2012-10-08 00:00:01 +1100

    Push notifications when deploying

--- zerobot-deploy.sh
+++ zerobot-deploy.sh
@@ -4,5 +4,5 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $scriptDir
 echo `date` Beginning deploy of $1-$2 from $3 >> /var/log/zerobot-deploy.log
-ansible 127.0.0.1 -c local --module-path=$scriptDir -m zerobot-deploy -a "project=$1 app=$2 bucket=$3 topic=$4 region=$5" >> /var/log/zerobot-deploy.log 2>&1
+ansible 127.0.0.1 -c local --module-path=$scriptDir -m zerobot-deploy -a "project=$1 app=$2 environment=$3 bucket=$4 topic=$5 region=$6" >> /var/log/zerobot-deploy.log 2>&1
 echo `date` Deploy of $1-$2 from $3 complete >> /var/log/zerobot-deploy.log
